5e9e4b645dd4903f8abe6a8ea33e450b3a2a482f
YuYue525 <58425611+YuYue525@users.noreply.github.com>
2021-03-05 17:44:23 +0800

    Add files via upload

--- viewSynthesis_bonus.cpp
+++ viewSynthesis_bonus.cpp
@@ -74,27 +74,27 @@ int main(int argc, char** argv)
                 double a, b;
                 
                 double X, Y;
-                X = Vx + (Vz*(2*Image_Width*c-Image_Width*(Resolution_Col-1)))/(double)(2*Resolution_Col)/(double)(targetFocalLen);
-                Y = Vy + (Vz*(2*Image_Height*r-Image_Height*(Resolution_Row-1)))/(double)(2*Resolution_Row)/(double)(targetFocalLen);
+                X = Vx + (Vz*(70*c-17885))/102400.0;
+                Y = Vy + (Vz*(70*r-17885))/102400.0;
                 
-                if(X < (Baseline*(View_Grid_Col-1)/2.0) && X>=-(Baseline*(View_Grid_Col-1)/2.0) && Y < =(Baseline*(View_Grid_Row-1)/2.0) && Y > -(Baseline*(View_Grid_Row-1)/2.0 ))
+                if(X<120 && X>=-120 && Y<=120 && Y>-120)
                 {
                     
-                    x = (int)(X + (Baseline*(View_Grid_Col-1)/2.0))/Baseline;
-                    y = (int)((Baseline*(View_Grid_Row-1)/2.0) - Y)/Baseline;
+                    x = (int)(X + 120)/Baseline;
+                    y = (int)(120 - Y)/Baseline;
 
-                    a = (X + (Baseline*(View_Grid_Col-1)/2.0) - (x * Baseline))/(double)Baseline;
-                    b = ((Baseline*(View_Grid_Row-1)/2.0) - Y - (y * Baseline))/(double)Baseline;
+                    a = (X + 120 - (x * Baseline))/(double)Baseline;
+                    b = (120 - Y - (y * Baseline))/(double)Baseline;
       
                     unsigned char red1, green1, blue1;
                     unsigned char red2, green2, blue2;
                     unsigned char red3, green3, blue3;
                     unsigned char red4, green4, blue4;
       
-                    viewImageList[View_Grid_Col * y + x].getColor(c, r, red1, green1, blue1);
-                    viewImageList[View_Grid_Col * y + (x+1)].getColor(c, r, red2, green2, blue2);
-                    viewImageList[View_Grid_Col * (y+1) + x].getColor(c, r, red3, green3, blue3);
-                    viewImageList[View_Grid_Col * (y+1) + (x+1)].getColor(c, r, red4, green4, blue4);
+                    viewImageList[9 * y + x].getColor(c, r, red1, green1, blue1);
+                    viewImageList[9 * y + (x+1)].getColor(c, r, red2, green2, blue2);
+                    viewImageList[9 * (y+1) + x].getColor(c, r, red3, green3, blue3);
+                    viewImageList[9 * (y+1) + (x+1)].getColor(c, r, red4, green4, blue4);
       
                     rayRGB.x = (double)((1-b) * ((1-a) * red1 + a * red2) + b * ((1-a) * red3 + a * red4));
                     rayRGB.y = (double)((1-b) * ((1-a) * green1 + a * green2) + b * ((1-a) * green3 + a * green4));
@@ -105,29 +105,28 @@ int main(int argc, char** argv)
             {
                 int x, y;
                 double a, b, X, Y;
-                //the position of the intersection
-                X = Vx + (Vz*(2*Image_Width*c-Image_Width*(Resolution_Col-1)))/(double)(2*Resolution_Col)/(double)(targetFocalLen);
-                Y = Vy + (Vz*(2*Image_Height*r-Image_Height*(Resolution_Row-1)))/(double)(2*Resolution_Row)/(double)(targetFocalLen);
-                              
-                x = (int)(X + (Baseline*(View_Grid_Col-1)/2.0))/Baseline;
-                y = (int)((Baseline*(View_Grid_Row-1)/2.0) - Y)/Baseline;
 
-                // alpha and beta of the position of the intersection
-                a = (X + (Baseline*(View_Grid_Col-1)/2.0) - (x * Baseline))/(double)Baseline;
-                b = ((Baseline*(View_Grid_Row-1)/2.0) - Y - (y * Baseline))/(double)Baseline;
+                X = Vx + (Vz*(70*c-17885))/1024.0/(double)targetFocalLen;//the position of the intersection
+                Y = Vy + (Vz*(70*r-17885))/1024.0/(double)targetFocalLen;
+                              
+                x = (int)(X + 120)/Baseline;
+                y = (int)(120 - Y)/Baseline;
+
+                a = (X + 120 - (x * Baseline))/(double)Baseline;// alpha and beta of the position of the intersection
+                b = (120 - Y - (y * Baseline))/(double)Baseline;
                 
                 int ci, ri;
                 double u, v, alpha, beta;
-                u = (double)(2*Image_Width*c-Image_Width*(Resolution_Col-1))*Focal_Length/(double)(2*Resolution_Col)/(double)targetFocalLen + Image_Width/2.0;// the position of the pixel
-                v = (double)(2*Image_Height*r-Image_Height*(Resolution_Row-1))*Focal_Length/(double)(2*Resolution_Row)/(double)targetFocalLen + Image_Height/2.0;
+                u = (double)(70*c-17885)*100.0/1024.0/(double)targetFocalLen + 17.5;// the position of the pixel
+                v = (double)(70*r-17885)*100.0/1024.0/(double)targetFocalLen + 17.5;
 
-                ci = (int)((double)((2*Resolution_Col) * u - Image_Width))/(double)(2*Image_Width);
-                ri = (int)((double)((2*Resolution_Row) * v - Image_Height))/(double)(2*Image_Height);
+                ci = (int)((double)(1024 * u - 35.0))/(70.0);
+                ri = (int)((double)(1024 * v - 35.0))/(70.0);
                 
-                alpha = ((2*Resolution_Col) * u - ((2*Image_Width) * ci + Image_Width)) / (double)(2*Image_Width);
-                beta = 1 - ((2*Resolution_Row) * v - (2*Image_Height * ri + Image_Height)) / (double)(2*Image_Height);
+                alpha = (1024 * u - (70 * ci + 35)) / (70.0);
+                beta = 1 - (1024 * v - (70 * ri + 35)) / (70.0);
                 
-                if(X < (Baseline*(View_Grid_Col-1)/2.0) && X>=-(Baseline*(View_Grid_Col-1)/2.0) && Y < =(Baseline*(View_Grid_Row-1)/2.0) && Y > -(Baseline*(View_Grid_Row-1)/2.0 ) && ci>=0 && ci<(Resolution_Col-1) && ri>=0 && ri<(Resolution_Row-1))
+                if(X<120 && X>=-120 && Y<=120 && Y>-120 && ci>=0 && ci<511 && ri>=0 && ri<511)
                 {
                     unsigned char red1, green1, blue1;// four rays of neighbour viewpoints
                     unsigned char red2, green2, blue2;
@@ -143,10 +142,10 @@ int main(int argc, char** argv)
                             unsigned char r3, g3, b3;
                             unsigned char r4, g4, b4;
                             
-                            viewImageList[View_Grid_Col * (y+i) + (x+j)].getColor(ci, ri+1, r1, g1, b1);
-                            viewImageList[View_Grid_Col * (y+i) + (x+j)].getColor(ci+1, ri+1, r2, g2, b2);
-                            viewImageList[View_Grid_Col * (y+i) + (x+j)].getColor(ci, ri, r3, g3, b3);
-                            viewImageList[View_Grid_Col * (y+i) + (x+j)].getColor(ci+1, ri, r4, g4, b4);
+                            viewImageList[9 * (y+i) + (x+j)].getColor(ci, ri+1, r1, g1, b1);
+                            viewImageList[9 * (y+i) + (x+j)].getColor(ci+1, ri+1, r2, g2, b2);
+                            viewImageList[9 * (y+i) + (x+j)].getColor(ci, ri, r3, g3, b3);
+                            viewImageList[9 * (y+i) + (x+j)].getColor(ci+1, ri, r4, g4, b4);
                             
                             if(i == 0 && j ==0)
                             {
@@ -190,10 +189,11 @@ int main(int argc, char** argv)
 	targetView.save(savePath.c_str());
 	cout << "Result saved!" << endl;
     
-    printf("do you want to generate a html with 3D display? [y/n]\n");
+    printf("1. do you want to generate a html with 3D display? [y/n]\n");
     char input;
     scanf("%c", &input);
-    if(input == 'c')
+    getchar();
+    if(input == 'y')
     {
         for(int new_X = -120; new_X<120; new_X += 10)
         {
@@ -269,6 +269,144 @@ int main(int argc, char** argv)
         fprintf(html_file, "</body>\n</html>\n");
     }
     
+    printf("2. do you want to update some basic parameters? [y/n]\n");
+    scanf("%c", &input);
+    getchar();
+    if(input == 'y')
+    {
+        printf("please enter the parameter as \n<viewpoint_coord> <focal_length> <baseline> <original_focal_length> <img_width> <img_height>\n");
+        int Vx, Vy, Vz, targetFocalLen, baseline, focal_length, image_width, image_height;
+        scanf("%d %d %d %d %d %d %d %d",&Vx, &Vy, &Vz, &targetFocalLen, &baseline, &focal_length, &image_width, &image_height);
+        Bitmap targetView(Resolution_Col, Resolution_Row);
+        for (int r = 0; r < Resolution_Row; r++)
+        {
+            for (int c = 0; c < Resolution_Col; c++)
+            {
+                Point3d rayRGB(0, 0, 0);
+                //! resample the pixel value of this ray: TODO
+                if(targetFocalLen == focal_length)//no need to bilinear interpolate pixels at one viewpoint
+                {
+                    int x, y;
+                    double a, b;
+                    
+                    double X, Y;
+                    X = Vx + (Vz*(2*image_width*c-image_width*(Resolution_Col-1)))/(double)(2*Resolution_Col)/(double)(targetFocalLen);
+                    Y = Vy + (Vz*(2*image_height*r-image_height*(Resolution_Row-1)))/(double)(2*Resolution_Row)/(double)(targetFocalLen);
+                    
+                    if(X < (baseline*(View_Grid_Col-1)/2.0) && X>=-(baseline*(View_Grid_Col-1)/2.0) && Y <=(baseline*(View_Grid_Row-1)/2.0) && Y > -(baseline*(View_Grid_Row-1)/2.0 ))
+                    {
+                        
+                        x = (int)(X + (baseline*(View_Grid_Col-1)/2.0))/baseline;
+                        y = (int)((baseline*(View_Grid_Row-1)/2.0) - Y)/baseline;
+
+                        a = (X + (baseline*(View_Grid_Col-1)/2.0) - (x * baseline))/(double)baseline;
+                        b = ((baseline*(View_Grid_Row-1)/2.0) - Y - (y * baseline))/(double)baseline;
+          
+                        unsigned char red1, green1, blue1;
+                        unsigned char red2, green2, blue2;
+                        unsigned char red3, green3, blue3;
+                        unsigned char red4, green4, blue4;
+          
+                        viewImageList[View_Grid_Col * y + x].getColor(c, r, red1, green1, blue1);
+                        viewImageList[View_Grid_Col * y + (x+1)].getColor(c, r, red2, green2, blue2);
+                        viewImageList[View_Grid_Col * (y+1) + x].getColor(c, r, red3, green3, blue3);
+                        viewImageList[View_Grid_Col * (y+1) + (x+1)].getColor(c, r, red4, green4, blue4);
+          
+                        rayRGB.x = (double)((1-b) * ((1-a) * red1 + a * red2) + b * ((1-a) * red3 + a * red4));
+                        rayRGB.y = (double)((1-b) * ((1-a) * green1 + a * green2) + b * ((1-a) * green3 + a * green4));
+                        rayRGB.z = (double)((1-b) * ((1-a) * blue1 + a * blue2) + b * ((1-a) * blue3 + a * blue4));
+                    }
+                }
+                else
+                {
+                    int x, y;
+                    double a, b, X, Y;
+                    //the position of the intersection
+                    X = Vx + (Vz*(2*image_width*c-image_width*(Resolution_Col-1)))/(double)(2*Resolution_Col)/(double)(targetFocalLen);
+                    Y = Vy + (Vz*(2*image_height*r-image_height*(Resolution_Row-1)))/(double)(2*Resolution_Row)/(double)(targetFocalLen);
+                                  
+                    x = (int)(X + (baseline*(View_Grid_Col-1)/2.0))/baseline;
+                    y = (int)((baseline*(View_Grid_Row-1)/2.0) - Y)/baseline;
+
+                    // alpha and beta of the position of the intersection
+                    a = (X + (baseline*(View_Grid_Col-1)/2.0) - (x * baseline))/(double)baseline;
+                    b = ((baseline*(View_Grid_Row-1)/2.0) - Y - (y * baseline))/(double)baseline;
+                    
+                    int ci, ri;
+                    double u, v, alpha, beta;
+                    u = (double)(2*image_width*c-image_width*(Resolution_Col-1))*focal_length/(double)(2*Resolution_Col)/(double)targetFocalLen + image_width/2.0;// the position of the pixel
+                    v = (double)(2*image_height*r-image_height*(Resolution_Row-1))*focal_length/(double)(2*Resolution_Row)/(double)targetFocalLen + image_height/2.0;
+
+                    ci = (int)((double)((2*Resolution_Col) * u - image_width))/(double)(2*image_width);
+                    ri = (int)((double)((2*Resolution_Row) * v - image_height))/(double)(2*image_height);
+                    
+                    alpha = ((2*Resolution_Col) * u - ((2*image_width) * ci + image_width)) / (double)(2*image_width);
+                    beta = 1 - ((2*Resolution_Row) * v - (2*image_height * ri + image_height)) / (double)(2*image_height);
+                    
+                    if(X < (baseline*(View_Grid_Col-1)/2.0) && X>=-(baseline*(View_Grid_Col-1)/2.0) && Y <=(baseline*(View_Grid_Row-1)/2.0) && Y > -(baseline*(View_Grid_Row-1)/2.0 ) && ci>=0 && ci<(Resolution_Col-1) && ri>=0 && ri<(Resolution_Row-1))
+                    {
+                        unsigned char red1, green1, blue1;// four rays of neighbour viewpoints
+                        unsigned char red2, green2, blue2;
+                        unsigned char red3, green3, blue3;
+                        unsigned char red4, green4, blue4;
+                        
+                        for(int i = 0; i<2;i++)
+                        {
+                            for(int j = 0; j<2; j++)
+                            {
+                                unsigned char r1, g1, b1;
+                                unsigned char r2, g2, b2;
+                                unsigned char r3, g3, b3;
+                                unsigned char r4, g4, b4;
+                                
+                                viewImageList[View_Grid_Col * (y+i) + (x+j)].getColor(ci, ri+1, r1, g1, b1);
+                                viewImageList[View_Grid_Col * (y+i) + (x+j)].getColor(ci+1, ri+1, r2, g2, b2);
+                                viewImageList[View_Grid_Col * (y+i) + (x+j)].getColor(ci, ri, r3, g3, b3);
+                                viewImageList[View_Grid_Col * (y+i) + (x+j)].getColor(ci+1, ri, r4, g4, b4);
+                                
+                                if(i == 0 && j ==0)
+                                {
+                                    red1 = (double)((1-beta) * ((1-alpha) * r1 + alpha * r2) + beta * ((1-alpha) * r3 + alpha * r4));
+                                    green1 = (double)((1-beta) * ((1-alpha) * g1 + alpha * g2) + beta * ((1-alpha) * g3 + alpha * g4));
+                                    blue1 = (double)((1-beta) * ((1-alpha) * b1 + alpha * b2) + beta * ((1-alpha) * b3 + alpha * b4));
+                                }
+                                else if(i == 0 && j == 1)
+                                {
+                                    red2 = (double)((1-beta) * ((1-alpha) * r1 + alpha * r2) + beta * ((1-alpha) * r3 + alpha * r4));
+                                    green2 = (double)((1-beta) * ((1-alpha) * g1 + alpha * g2) + beta * ((1-alpha) * g3 + alpha * g4));
+                                    blue2 = (double)((1-beta) * ((1-alpha) * b1 + alpha * b2) + beta * ((1-alpha) * b3 + alpha * b4));
+                                }
+                                else if(i ==1 && j == 0)
+                                {
+                                    red3 = (double)((1-beta) * ((1-alpha) * r1 + alpha * r2) + beta * ((1-alpha) * r3 + alpha * r4));
+                                    green3 = (double)((1-beta) * ((1-alpha) * g1 + alpha * g2) + beta * ((1-alpha) * g3 + alpha * g4));
+                                    blue3 = (double)((1-beta) * ((1-alpha) * b1 + alpha * b2) + beta * ((1-alpha) * b3 + alpha * b4));
+                                }
+                                else
+                                {
+                                    red4 = (double)((1-beta) * ((1-alpha) * r1 + alpha * r2) + beta * ((1-alpha) * r3 + alpha * r4));
+                                    green4 = (double)((1-beta) * ((1-alpha) * g1 + alpha * g2) + beta * ((1-alpha) * g3 + alpha * g4));
+                                    blue4 = (double)((1-beta) * ((1-alpha) * b1 + alpha * b2) + beta * ((1-alpha) * b3 + alpha * b4));
+                                }
+                            }
+                        }
+                        
+                        rayRGB.x = (double)((1-b) * ((1-a) * red1 + a * red2) + b * ((1-a) * red3 + a * red4));
+                        rayRGB.y = (double)((1-b) * ((1-a) * green1 + a * green2) + b * ((1-a) * green3 + a * green4));
+                        rayRGB.z = (double)((1-b) * ((1-a) * blue1 + a * blue2) + b * ((1-a) * blue3 + a * blue4));
+                    }
+                }
+                
+                //! record the resampled pixel value
+                targetView.setColor(c, r, (unsigned char) rayRGB.x, (unsigned char) rayRGB.y, (unsigned char) rayRGB.z);
+            }
+        }
+        
+        string savePath = "newParameterView.bmp";
+        targetView.save(savePath.c_str());
+        cout << "Result saved!" << endl;
+        
+    }
             
 
 	return 0;
